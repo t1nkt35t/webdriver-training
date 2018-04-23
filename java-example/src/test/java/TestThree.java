a9a436ca63453828d9cd4746b164b51c316e454c
Stanislav <t1nkt35t@gmail.com>
2018-04-23 23:18:54 +0300

    3
    3

--- java-example/src/test/java/TestThree.java
+++ java-example/src/test/java/TestThree.java
@@ -3,12 +3,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class TestTwo {
+public class TestThree {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -16,16 +20,28 @@ public class TestTwo {
     public void start() {
         System.setProperty("webdriver.chrome.driver", "E:/_A_R_C_H_i_v_e/Java/_wdlearn/chromedriver_win32/chromedriver.exe");
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver,10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        wait = new WebDriverWait(driver,10);
     }
 
     @Test
-    public void TestTwo() {
+    public void TestThree() {
         driver.get("http://localhost/litecart/admin");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        wait.until(titleIs("My Store"));
+        List<WebElement> menu = driver.findElements(By.xpath("//td[@id='sidebar']//li[@id='app-']"));
+        for (WebElement item : menu) {
+            item.click();
+            assert(driver.findElement(By.xpath("//td[@id='content']//h1"))).isDisplayed();
+            List<WebElement> submenu = item.findElements(By.xpath(".//ul[@class='docs']//a"));
+            if (submenu.size() > 0 ) {
+                for (WebElement subitem:submenu) {
+                    subitem.click();
+                    assert(driver.findElement(By.xpath("//td[@id='content']//h1"))).isDisplayed();
+                }
+            }
+        }
     }
 
     @After
@@ -34,3 +50,4 @@ public class TestTwo {
         driver= null;
     }
 }
+
