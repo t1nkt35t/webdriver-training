e339d11fa9eda6303aa6a000ce1eda76a1dca881
Stanislav <t1nkt35t@gmail.com>
2018-05-07 17:02:51 +0300

    7
    7

---
+++ java-example/src/test/java/test7.java
@@ -0,0 +1,54 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.base.Verify.verify;
+
+public class test7 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "c:/_java/chromedriver_win32/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "E:/_A_R_C_H_i_v_e/Java/_wdlearn/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void test7() {
+
+        driver.get("http://localhost/litecart");
+        driver.findElement(By.xpath("//form[@name=\"login_form\"]//a[contains(text(), \"New customers click here\")]")).click();
+        driver.findElement(By.xpath("//input[@name = \"firstname\"]")).sendKeys("Test" +
+                Keys.TAB + "Testov" + Keys.TAB + "prospekt mira, 10-1" + Keys.TAB + Keys.TAB + 53312 + Keys.TAB + "Moscow");
+        Select dropdownCountry = new Select(driver.findElement(By.name("country_code")));
+        dropdownCountry.selectByVisibleText("United States");
+//        Select dropdownState = new Select(driver.findElement(By.name("zone_code")));
+//        dropdownCountry.selectByIndex(1);
+////        selectByVisibleText("Alaska");
+//        driver.findElement(By.name("zone_code")).click();
+//        driver.findElement(By.xpath("//select[@name=\"zone_code\"]//option[@value=\"AK\"]")).click();
+        driver.findElement(By.xpath("//input[@name=\"e-mail\"]")).sendKeys("lil@yaz.ru" +
+                Keys.TAB + "+78754334332" + Keys.TAB + Keys.SPACE + Keys.TAB + "qwe123" + Keys.TAB +
+                "qwe123" + Keys.TAB + Keys.ENTER);
+    }
+
+    @After
+    public void stop() {
+//        driver.quit();
+        driver= null;
+    }
+}
